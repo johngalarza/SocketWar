12863e007f12c213c66d03c81c7750e966a78209
johngalarza <johngalarza1970@gmail.com>
2025-11-02 22:42:30 -0500

    feat: Rotation of controllers player

--- public/index.js
+++ public/index.js
@@ -22,7 +22,10 @@ async function renderScenery() {
 
 async function renderPlayer(){
     const texture = await Assets.load('./assets/tank.png');
-    player = new Sprite(texture);
+    player = new Sprite({
+        texture,
+        anchor: 0.5
+    });
     player.x = 0;
     player.y = 0;
     container.addChild(player)
@@ -32,16 +35,19 @@ function controllers(){
     document.addEventListener('keydown', (e)=>{
         if(e.key==='w'){
             player.y -= 5;
-            player.rotation = 2
+            player.rotation = 0;
         }
         if(e.key==='d'){
             player.x += 5;
+            player.rotation = Math.PI / 2;
         }
         if(e.key==='s'){
             player.y += 5;
+            player.rotation = Math.PI;
         }
         if(e.key==='a'){
             player.x -= 5;
+            player.rotation = Math.PI + Math.PI /2;
         }
     })
 }
